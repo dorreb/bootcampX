0c93f2556b6b47fbc6b39ecabf15762429270eb4
dorreb <ashley_ure@hotmail.com>
2023-02-07 14:06:04 -0500

    more querie practice using psql

---
+++ 1_total_teacher_assistance_requests.sql
@@ -0,0 +1,8 @@
+SELECT
+  COUNT(assistance_requests.*) as total_assistances,
+  teachers.name as name
+FROM
+  assistance_requests
+  JOIN teachers ON teachers.id = teacher_id
+WHERE
+  name = 'Waylon Boehm';
---
+++ 4_queries/6_cohort_with_longest_assistances.sql
@@ -0,0 +1,13 @@
+SELECT
+  cohorts.name,
+  AVG(completed_at - started_at) as average_assistance_time
+FROM
+  assistance_requests
+  JOIN students on students.id = assistance_requests.student_id
+  JOIN cohorts on cohorts.id = cohort_id
+GROUP BY
+  cohorts.name
+ORDER BY
+  average_assistance_time DESC
+LIMIT
+  1;
